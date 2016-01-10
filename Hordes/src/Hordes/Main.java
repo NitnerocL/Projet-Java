914580a9b4eebd3fb9bb544f53cd41a68c09bf91
NitnerocL <corle@live.fr>
2016-01-10 23:41:43 +0100

    classe main

---
+++ Hordes/src/Hordes/Main.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Hordes;
+
+/**
+ *
+ * @author Corentin
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("                                                                 .....     \n" +
+"                                                                C C  /     \n" +
+"                                                               /<   /      \n" +
+"                                                ___ __________/_#__=o      \n" +
+"                                               /(- /(\\_\\________   \\       \n" +
+"                                               \\ ) \\ )_      \\o     \\      \n" +
+"                                               /|\\ /|\\       |'     |      \n" +
+"                                                             |     _|      \n" +
+"                                                             /o   __\\      \n" +
+" ▄▀▀▄ ▄▄   ▄▀▀▀▀▄   ▄▀▀▄▀▀▀▄  ▄▀▀█▄▄   ▄▀▀█▄▄▄▄  ▄▀▀▀▀▄     / '     |      \n" +
+"█  █   ▄▀ █      █ █   █   █ █ ▄▀   █ ▐  ▄▀   ▐ █ █   ▐    / /      |      \n" +
+"▐  █▄▄▄█  █      █ ▐  █▀▀█▀  ▐ █    █   █▄▄▄▄▄     ▀▄     /_/\\______|      \n" +
+"   █   █  ▀▄    ▄▀  ▄▀    █    █    █   █    ▌  ▀▄   █   (   _(    <       \n" +
+"  ▄▀  ▄▀    ▀▀▀▀   █     █    ▄▀▄▄▄▄▀  ▄▀▄▄▄▄    █▀▀▀     \\    \\    \\      \n" +
+" █   █             ▐     ▐   █     ▐   █    ▐    ▐         \\    \\    |     \n" +
+" ▐   ▐                       ▐         ▐                    \\____\\____\\    \n" +
+"                                                            ____\\_\\__\\_\\   ");
+    }
+    
+}
